52fa0614a5c69d97ef5804e923dad9aeae675f53
rheydne <rheydne@gmail.com>
2023-07-08 23:46:48 -0300

    Configuracao Inicial

---
+++ src/main/java/com/rheydne/monitoringspending/MonitoringspendingApplication.java
@@ -0,0 +1,13 @@
+package com.rheydne.monitoringspending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringspendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringspendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rheydne/monitoringspending/MonitoringspendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rheydne.monitoringspending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringspendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
