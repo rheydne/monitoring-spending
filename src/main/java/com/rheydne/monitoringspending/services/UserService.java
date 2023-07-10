cd928a9895aeeafce6aa87de328a35600ac787da
rheydne <rheydne@gmail.com>
2023-07-10 12:10:14 -0300

    Implementacao métodos basicos, com a regra de negocio mascarando o retorno do email

---
+++ src/main/java/com/rheydne/monitoringspending/services/UserService.java
@@ -0,0 +1,72 @@
+package com.rheydne.monitoringspending.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rheydne.monitoringspending.models.User;
+import com.rheydne.monitoringspending.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    // public User findById(Long id) {
+    //     Optional<User> user = this.userRepository.findById(id);
+
+    //     return user.orElseThrow(() -> new RuntimeException(
+    //         "Usuário não encontrado! id: " + id + ", Tipo: " + User.class.getName()
+    //     ));
+    // }
+
+    public User findById(Long id) {
+        User user;
+        Optional<User> obj = this.userRepository.findById(id);
+        user = obj.orElse(null);
+        if(user == null) {
+            throw new RuntimeException("Usuário não encontrado! id: " + id + ", Tipo: " + User.class.getName());
+        } else {
+            user.setEmailAdress(maskedEmail(user.getEmailAdress()));
+        }
+        return user;
+    }
+
+    @Transactional
+    public User create(User user) {
+        user.setId(null);
+        user = this.userRepository.save(user);
+        return user;
+    }
+
+    @Transactional
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(user.getPassword());
+        newUser.setFullName(user.getFullName());
+        newUser.setDateBirth(user.getDateBirth());
+        newUser.setEmailAdress(user.getEmailAdress());
+        return this.userRepository.save(newUser);
+    }
+
+    public void delete(Long id) {
+        try {
+            findById(id);
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Não foi possível excluir o usuário!");
+        }
+    }
+
+    private String maskedEmail(String email) {
+        int arrobaIndex = email.indexOf("@");
+        String lengh = email.substring(2, arrobaIndex);
+        String masked = "*".repeat(lengh.length());
+        return email.substring(0, 2) + masked + email.substring(arrobaIndex);
+    }
+}
